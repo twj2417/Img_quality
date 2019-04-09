9971a9ec4d220270adbe5132b4988e7cd4aaaaee
Weijie Tao <37058945+twj2417@users.noreply.github.com>
2019-04-09 10:15:17 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='img-quality',
+      version='0.0.1',
+      description='Image quality index.',
+      url='https://github.com/twj2417/Img_quality',
+      author='Tao Weijie',
+      author_email='twj2417@gmail.com',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+          'fs',
+          'click',
+      ],
+      entry_points='''
+            [console_scripts]
+            dxcluster=Img_quality.cli.main:imgq
+      ''',
+      scripts=[],
+      zip_safe=False)
